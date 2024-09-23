af4d817188ecdca25c7fb797e639b2d10561194c
Arda <loljoket@gmail.com>
2024-09-24 00:59:13 +0300

    Fixed directory creation

--- src/main/java/xyz/geik/farmer/modules/spawnerkiller/SpawnerKiller.java
+++ src/main/java/xyz/geik/farmer/modules/spawnerkiller/SpawnerKiller.java
@@ -94,7 +94,7 @@ public class SpawnerKiller extends FarmerModule {
     public void setupFile() {
         configFile = ConfigManager.create(ConfigFile.class, (it) -> {
             it.withConfigurer(new YamlBukkitConfigurer());
-            it.withBindFile(new File(Main.getInstance().getDataFolder(), String.format("/modules/%s/config.yml", getName())));
+            it.withBindFile(new File(Main.getInstance().getDataFolder(), String.format("/modules/%s/config.yml", getName().toLowerCase())));
             it.saveDefaults();
             it.load(true);
         });
